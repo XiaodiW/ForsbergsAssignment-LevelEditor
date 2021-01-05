91adfccacca0e9c34c4cf003661f3cb6b523e3bf
Xiaodi Wang <xiaodi.wang@student.forsbergsskola.se>
2021-01-05 16:05:54 +0100

    Change location typesUI

---
+++ Assets/Script/TypesUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TypesUI : MonoBehaviour
+{
+    public TileType _tileTypeSelected;
+
+    public void Selected (TileType _tileTypeDest)
+    {
+        _tileTypeSelected = _tileTypeDest;
+        Debug.Log($"TileType Name is {_tileTypeSelected.name}");
+    }
+}
