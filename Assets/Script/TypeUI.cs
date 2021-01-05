a2db5633c355cbfa5c20a2c450a013e939bf61fd
Xiaodi Wang <xiaodi.wang@student.forsbergsskola.se>
2021-01-05 15:56:53 +0100

    Add TileType Prefab and UI script

---
+++ Assets/Script/TypeUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeUI : MonoBehaviour
+{
+    public TileType tileType;
+    private TypesUI _typesUI;
+    // public GameObject background;
+    // public string name;
+    // public Color color;
+    public Text nameUI;
+    private Image image;
+    private Button _button;
+    private Text buttonText;
+
+    private void Start()
+    {
+        image = GetComponentInChildren<Image>();
+        _typesUI = GetComponentInParent<TypesUI>();
+        _button = GetComponentInChildren<Button>();
+        buttonText = _button.GetComponentInChildren<Text>();
+    }
+
+    private void Update()
+    {
+        image.color = tileType.color;
+        nameUI.text = tileType.name;
+        buttonText.text = (_typesUI._tileTypeSelected == this.tileType) ? "Selected" : "Select";
+        // background.SetActive(_typesUI._tileTypeSelected == this.tileType);
+
+    }
+
+    public void OnButtonDown()
+    {
+        _typesUI.Selected(this.tileType);
+    }
+}
