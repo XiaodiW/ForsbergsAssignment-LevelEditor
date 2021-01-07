a0363fd001a694dd31478026c373474b9aefccfe
Xiaodi Wang <xiaodi.wang@student.forsbergsskola.se>
2021-01-07 12:57:42 +0100

    Add Save Level Name

--- Assets/Script/DataManager.cs
+++ Assets/Script/DataManager.cs
@@ -1,5 +1,8 @@
 using System;
+using System.IO;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Script
 {
@@ -14,15 +17,29 @@ namespace Script
     {
         public TypesUI typesUI;
         public MapUI mapUI;
-        
+        public InputField nameInput;
+
+        private void Start()
+        {
+            nameInput.gameObject.SetActive(false);
+            nameInput.onEndEdit.AddListener(ToSave);
+        }
+
         public void OnSaving()
         {
+            nameInput.gameObject.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(nameInput.gameObject,null);
+        }
+
+        private void ToSave(String name)
+        {
+            nameInput.gameObject.SetActive(false);
             var level = new Level();
-            level.name = "Level 1";
+            level.name = name;
             level.typesJson = typesUI.ToSaveTypes();
             level.mapJson = mapUI.ToSaveTiles();
             var saveData = JsonUtility.ToJson(level, true);
-            System.IO.File.WriteAllText(Application.persistentDataPath + "/Data.json", saveData);
+            File.WriteAllText(Application.persistentDataPath + "/Data.json", saveData);
         }
 
         public void OnLoading()
